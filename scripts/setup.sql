eca0751baeaa0c3d660548d66dfc45d5abc1bca5
Xuan Nguyen <109402524+XuanNguyenCU@users.noreply.github.com>
2024-03-17 04:54:15 -0400

    Create setup.sql

---
+++ scripts/setup.sql
@@ -0,0 +1,16 @@
+-- Xuan Nguyen 101228417
+
+-- Database Schema
+CREATE TABLE students (
+    student_id          SERIAL          PRIMARY KEY,
+    first_name          VARCHAR(255)    NOT NULL,
+    last_name           VARCHAR(255)    NOT NULL,
+    email               VARCHAR(255)    NOT NULL UNIQUE,
+    enrollment_date     DATE            DEFAULT CURRENT_DATE
+);
+
+-- Initial Sample Data
+INSERT INTO students (first_name, last_name, email, enrollment_date) VALUES
+('John', 'Doe', 'john.doe@example.com', '2023-09-01'),
+('Jane', 'Smith', 'jane.smith@example.com', '2023-09-01'),
+('Jim', 'Beam', 'jim.beam@example.com', '2023-09-02');
